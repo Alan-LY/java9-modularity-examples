c05f1bd3ee68289da1d48c9298970af3f17142d9
李阳 <liyang23@yiche.com>
2022-06-20 16:28:46 +0800

    m

--- chapter5/optional_dependencies_annotations/run.sh
+++ chapter5/optional_dependencies_annotations/run.sh
@@ -1,7 +1,9 @@
-javac --module-source-path src -d out -m application,schemagenerator
+export JAVA_HOME="/Users/liyang/projects/java9-modularity-examples/tmp/jfx"
+
+$JAVA_HOME/bin/javac --module-source-path src -d out -m application,schemagenerator
 
 # Exclude myannotations module from the run-time classpath
-java -ea --module-path out/application -m application/javamodularity.application.Main
+# $JAVA_HOME/bin/java -ea --module-path out/application -m application/javamodularity.application.Main
 
 # Include myannotations module, expected: AssertionError
-java -ea --module-path out --add-modules schemagenerator -m application/javamodularity.application.Main
+$JAVA_HOME/bin/java -ea --module-path out --add-modules schemagenerator -m application/javamodularity.application.Main
